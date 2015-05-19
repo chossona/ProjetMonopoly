a803228a5cc775b794e4836d4e63f229f2fc65a3
chossona <aurelien.chosson@gmail.com>
2015-05-19 09:31:51 +0200

    Salut

--- ProjetMonopoly/src/projetmonopoly/ProjetMonopoly.java
+++ ProjetMonopoly/src/projetmonopoly/ProjetMonopoly.java
@@ -17,6 +17,7 @@ public class ProjetMonopoly {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Salut ma gueuze !");
     }
     
 }
