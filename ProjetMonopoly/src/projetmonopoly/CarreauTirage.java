9989797bcda6f2e22a56be058e5c250a7637c524
chossona <aurelien.chosson@gmail.com>
2015-06-08 12:53:56 +0200

    chipolatas

--- ProjetMonopoly/src/projetmonopoly/CarreauTirage.java
+++ ProjetMonopoly/src/projetmonopoly/CarreauTirage.java
@@ -5,20 +5,33 @@ public class CarreauTirage extends CarreauAction {
     public CarreauTirage(int _numero, String _nomCarreau, Monopoly _monopoly) {
         super(_numero, _nomCarreau, _monopoly);
     }
-    public void TirerUneCarte(){
+
+    public void TirerUneCarte() {
         Carte c;
-        if (this.getNumero() == 7 ||this.getNumero() == 22 || this.getNumero() == 36 ){
+
+        if (this.getNumero() == 7 || this.getNumero() == 22 || this.getNumero() == 36) {
             c = this.getMonopoly().getCarteChance().getFirst();
             c.ActionCarte();
-        }
-        else if (this.getNumero() == 2 ||this.getNumero() == 17 || this.getNumero() == 33 ){
+            if (c.getNumCarte() == 1) {
+                this.getMonopoly().getCarteChance().remove(c);
+            } else {
+                this.getMonopoly().getCarteChance().remove(c);
+                this.getMonopoly().getCarteChance().offer(c);
+
+            }
+        } else if (this.getNumero() == 2 || this.getNumero() == 17 || this.getNumero() == 33) {
             c = this.getMonopoly().getCarteCaisse().getFirst();
             c.ActionCarte();
-        }
-      
-       
-        if (){
-            
+            if (c.getNumCarte() == 1) {
+                this.getMonopoly().getCarteCaisse().remove(c);
+            } else {
+                this.getMonopoly().getCarteCaisse().remove(c);
+                this.getMonopoly().getCarteCaisse().offer(c);
+
+            }
+
         }
     }
+
 }
+
