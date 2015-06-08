eada846f72e683ad735597ddbf7e8aa9d2c78626
chossona <aurelien.chosson@gmail.com>
2015-06-08 10:57:58 +0200

    master

--- ProjetMonopoly/src/projetmonopoly/CarreauTirage.java
+++ ProjetMonopoly/src/projetmonopoly/CarreauTirage.java
@@ -5,4 +5,12 @@ public class CarreauTirage extends CarreauAction {
     public CarreauTirage(int _numero, String _nomCarreau, Monopoly _monopoly) {
         super(_numero, _nomCarreau, _monopoly);
     }
+    public void TirerUneCarte(){
+        if (this.getNumero() == 7 ||this.getNumero() == 22 || this.getNumero() == 36 ){
+            this.getMonopoly().getCarteChance().getFirst().ActionCarte();
+        }
+        else if (this.getNumero() == 2 ||this.getNumero() == 17 || this.getNumero() == 33 ){
+            this.getMonopoly().getCarteCaisse().getFirst().ActionCarte();
+        }
+    }
 }
